73f573e54b9771c66001cf4ef2942e76e5509320
Tep1g <gamerluis.m@gmail.com>
2024-08-28 13:53:24 -0500

    add problem 2

---
+++ assignments/1/problem_2.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    usr_num = int(input("Enter a positive integer: "))
+    if usr_num < 0:
+        raise ValueError("Invalid integer {}".format(usr_num))
+
+    if usr_num == 0:
+        answer = 1
+    else:
+        answer = usr_num
+        for n in range(usr_num-1, 0, -1):
+            answer *= n
+
+    print("{}! = {}".format(usr_num, answer))
