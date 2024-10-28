d27f3e3e2af07036ac00137b9208864d59d999b7
Tep1g <gamerluis.m@gmail.com>
2024-10-28 12:39:53 -0500

    properly display part c histogram

--- src/hw-7/hw_7.py
+++ src/hw-7/hw_7.py
@@ -1,6 +1,6 @@
+from matplotlib import pyplot
 import numpy
 import pandas
-import matplotlib
 import psycopg2
 
 if __name__ == "__main__":
@@ -8,10 +8,11 @@ if __name__ == "__main__":
     # Number 1
     # Part a
     thistuple = ("apple", "banana", "cherry")
-    df = pandas.read_csv("/home/luis.maria/Documents/Git/intro-to-db/src/hw-7/train.csv")
+    df = pandas.read_csv("train.csv")
 
     # Part b
     print(df.head(10))
 
     # Part c
-    print(df['LoanAmount'].hist(bins=50))
+    histogram = df['LoanAmount'].hist(bins=50)
+    pyplot.show()
